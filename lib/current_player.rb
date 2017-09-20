b39f1980c9073f33a885ac5588f0049f3a0544cf
John Tuttle <thejwt@gmail.com>
2017-09-20 17:13:40 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  turns = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      turns += 1
+    end
+    return turns
+end
+
+def current_player(board)
+  turns = turn_count(board)
+  if turns == 0 || turns % 2 == 0
+    return "X"
+  else
+    return "O"
+end
